a373396acb18658241f0f54be29177eaa3ecd30e
stephylee <slemoine@slad.eu>
2016-07-19 17:42:03 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Shtumi\UsefulBundle\DependencyInjection;
+namespace Slad\UsefulBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('shtumi_useful');
+        $rootNode = $treeBuilder->root('slad_useful');
 
         $rootNode
 
