38cbbd604b120b4f1b0476281255a2e60900ca11
stephylee <slemoine@slad.eu>
2016-07-19 17:43:04 +0200

    Update and rename DependencyInjection/ShtumiUsefulExtension.php to DependencyInjectionSladUsefulExtension.php

--- DependencyInjectionSladUsefulExtension.php
+++ DependencyInjectionSladUsefulExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Shtumi\UsefulBundle\DependencyInjection;
+namespace Slad\UsefulBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class ShtumiUsefulExtension extends Extension
+class SladUsefulExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -22,9 +22,9 @@ class ShtumiUsefulExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setParameter('shtumi.autocomplete_entities', $config['autocomplete_entities']);
-        $container->setParameter('shtumi.dependent_filtered_entities', $config['dependent_filtered_entities']);
-        $container->setParameter('shtumi.date_range', $config['date_range']);
+        $container->setParameter('slad.autocomplete_entities', $config['autocomplete_entities']);
+        $container->setParameter('slad.dependent_filtered_entities', $config['dependent_filtered_entities']);
+        $container->setParameter('slad.date_range', $config['date_range']);
 
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
